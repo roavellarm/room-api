f579c0964dab981c9edcfd98dafc081f5cfd0752
Rodrigo Avellar <roavellarm@gmail.com>
2019-11-18 14:08:57 -0200

    Add user policy

--- app/controllers/user_controller.rb
+++ app/controllers/user_controller.rb
@@ -4,6 +4,7 @@ class UserController < ApplicationController
   before_action :authenticate_user!
 
   def index
+    authorize :user
     render status: :ok, json: User.all.order(first_name: :asc, last_name: :asc)
   end
 end
