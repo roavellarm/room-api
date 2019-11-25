a2384490516c88483fe08ed3482790c37400a948
Rodrigo Avellar <roavellarm@gmail.com>
2019-11-25 04:01:37 -0200

    Update test

--- spec/requests/user/change_mood_spec.rb
+++ spec/requests/user/change_mood_spec.rb
@@ -3,15 +3,27 @@
 require 'rails_helper'
 
 describe 'PUT /user/change_mood', type: :request do
-  # let(:user) { current_user }
-  let(:params) { { mood: 'tired' }.to_json }
+  let(:params) { { mood: 'tired' } }
+
+  let(:expected_hash) do
+    {
+      id: current_user.id,
+      first_name: 'Current',
+      last_name: 'User',
+      email: 'current.user@email.com',
+      mood: {
+        id: tired_mood.id,
+        name: 'tired'
+      }
+    }.to_json
+  end
 
   before do
     tired_mood
-    put '/user/change_mood', params: params
+    put '/user/change_mood', params: params.to_json
   end
 
-  it { expect(response).to have_http_status :ok }
-  # it { expect(response.body).to eq(expected_hash) }
-  # it { expect(JSON.parse(response.body).length).to eq(4) }
+  it { expect(response).to have_http_status(:ok) }
+  it { expect(response.body).to eq(expected_hash) }
+  it { expect(JSON.parse(response.body).length).to eq(5) }
 end
