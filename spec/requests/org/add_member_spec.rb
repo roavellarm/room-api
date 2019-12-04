1f24ed28011b4d91f6d0a7379849322a99b51140
Rodrigo Avellar <roavellarm@gmail.com>
2019-12-04 14:37:53 -0200

    Add more contexts to add_member_spec

--- spec/requests/org/add_member_spec.rb
+++ spec/requests/org/add_member_spec.rb
@@ -93,6 +93,99 @@ describe 'PUT /org/:id/add_member', type: :request do
       it { expect(response.body).to eq(expected_hash) }
       it { expect(org.reload.as_json.to_json).to eq(expected_hash) }
     end
+
+    describe 'when the user to be added is already a member of the org' do
+      let(:params) { { email: average_joe.email }.to_json }
+
+      let(:org) do
+        Org.create!(
+          user: average_sally,
+          name: 'Sallys Company',
+          description: 'Lorem ipsum dolor sit amet',
+          image: 'https://source.unsplash.com/random'
+        )
+      end
+
+      let(:expected_hash) do
+        {
+          id: org.id,
+          user: average_sally,
+          name: 'Sallys Company',
+          description: 'Lorem ipsum dolor sit amet',
+          image: 'https://source.unsplash.com/random',
+          rooms: [],
+          members: [
+            {
+              id: average_joe.id,
+              first_name: 'Joe',
+              last_name: 'Average',
+              email: 'joe@email.com',
+              image: nil,
+              mood: nil
+            },
+            {
+              id: current_user.id,
+              first_name: 'Current',
+              last_name: 'User',
+              email: 'current.user@email.com',
+              image: nil,
+              mood: nil
+            }
+          ]
+        }.to_json
+      end
+      let(:error_message) do
+        { success: false,
+          errors: { id: 'unprocessable_entity',
+                    message: 'The user is already a member of this org' } }
+      end
+
+      before do
+        org.members.append(average_joe)
+        org.members.append(current_user)
+        put "/org/#{org.id}/add_member", params: params
+      end
+
+      it { expect(response).to have_http_status(:unprocessable_entity) }
+      it { expect(response.body).to eq(error_message.to_json) }
+      it { expect(org.reload.as_json.to_json).to eq(expected_hash) }
+    end
+
+    describe 'when the user to be added is not register yet' do
+      let(:params) { { email: 'not_register@email.com' }.to_json }
+
+      let(:org) do
+        Org.create!(
+          user: current_user,
+          name: 'Current User Company',
+          description: 'Lorem ipsum dolor sit amet',
+          image: 'https://source.unsplash.com/random'
+        )
+      end
+
+      let(:expected_hash) do
+        {
+          id: org.id,
+          user: current_user,
+          name: 'Current User Company',
+          description: 'Lorem ipsum dolor sit amet',
+          image: 'https://source.unsplash.com/random',
+          rooms: [],
+          members: []
+        }.to_json
+      end
+      let(:error_message) do
+        { success: false,
+          errors: { id: 'not_found',
+                    message: 'The user is not registered already in the app' } }
+      end
+
+      before { put "/org/#{org.id}/add_member", params: params }
+
+      it { expect(response).to have_http_status(:not_found) }
+      it { expect(response.body).to eq(error_message.to_json) }
+      it { expect(org.reload.as_json.to_json).to eq(expected_hash) }
+    end
   end
 
   context 'without current_user' do
