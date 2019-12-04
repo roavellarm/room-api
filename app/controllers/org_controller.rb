11706e7628040f0688a50a7d4350434c2cd8042c
Rodrigo Avellar <roavellarm@gmail.com>
2019-12-04 14:37:12 -0200

    Add new filters to add member endpoint

--- app/controllers/org_controller.rb
+++ app/controllers/org_controller.rb
@@ -23,17 +23,42 @@ class OrgController < ApplicationController
     render json: org
   end
 
+  # rubocop:disable Metrics/AbcSize
   def add_member
     org = Org.find(params[:id])
     authorize org
     user = User.find_by(email: params[:email])
+    return user_not_register if user.nil?
+    return already_member if org.members.include?(user)
+
     org.members.append(user)
-    render status: :ok, json: org
+    render status: :ok, json: org.as_json
   end
+  # rubocop:enable Metrics/AbcSize
 
   private
 
   def permitted_params
     params.require(:org).permit!
+  end
+
+  def already_member
+    render status: :unprocessable_entity, json: error_message.to_json
+  end
+
+  def user_not_register
+    render status: :not_found, json: error_message2.to_json
+  end
+
+  def error_message
+    { success: false,
+      errors: { id: 'unprocessable_entity',
+                message: 'The user is already a member of this org' } }
+  end
+
+  def error_message2
+    { success: false,
+      errors: { id: 'not_found',
+                message: 'The user is not registered already in the app' } }
   end
 end
